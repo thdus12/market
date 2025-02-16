421da8008310a834b1be0208204dd15728f3e95b
배소연 <bthdus5890@gmail.com>
2025-02-16 20:42:43 +0900

    fix: request 유효성 체크 추가

--- src/main/java/com/allra/market/domain/payment/model/dto/request/PostPaymentRequest.java
+++ src/main/java/com/allra/market/domain/payment/model/dto/request/PostPaymentRequest.java
@@ -1,13 +1,19 @@
 package com.allra.market.domain.payment.model.dto.request;
 
 import com.allra.market.domain.payment.type.PaymentType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PostPaymentRequest {
+    @NotNull
     private PaymentType type;
+
+    @NotNull
+    @Min(1)
     private Long totalAmount;
 
     public PostPaymentRequest(PaymentType type, Long totalAmount) {
